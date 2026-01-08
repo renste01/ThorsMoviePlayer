8a0ce2799bce8193ed5e9e70b91ea4ccdf3b1322
rekast <renste01@easv365.dk>
2026-01-08 14:01:20 +0100

    Database sat op i 3-lager arkitekturen

--- src/main/java/dk/easv/thorsmovieplayer/BE/Movie.java
+++ src/main/java/dk/easv/thorsmovieplayer/BE/Movie.java
@@ -1,4 +1,50 @@
 package dk.easv.thorsmovieplayer.BE;
 
-public class Movie {
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+public class Movie
+{
+    private int id;
+    private String title;
+    private String filePath;
+    private LocalDateTime lastView;
+    private float personalRating;
+    private float imdbRating;
+
+    public Movie(int id, String title, String filePath, LocalDateTime lastView, float personalRating, float imdbRating)
+    {
+        this.id = id;
+        this.title = title;
+        this.filePath = filePath;
+        this.lastView = lastView;
+        this.personalRating = personalRating;
+        this.imdbRating = imdbRating;
+    }
+
+    // Getters
+    public int getId() {return id;}
+
+    public String getTitle() {return title;}
+
+    public String getFilePath() {return filePath;}
+
+    public LocalDateTime getLastView() {return lastView;}
+
+    public float getPersonalRating() {return personalRating;}
+
+    public float getImdbRating() {return imdbRating;}
+
+    // Setters
+    public void setId(int id) {this.id = id;}
+
+    public void setTitle(String title) {this.title = title;}
+
+    public void setFilepath(String filePath) {this.filePath = filePath;}
+
+    public void setLastView(LocalDateTime lastView) {this.lastView = lastView;}
+
+    public void setPersonalRating(float personalRating) {this.personalRating = personalRating;}
+
+    public void setImdbRating(float imdbRating) {this.imdbRating = imdbRating;}
 }
--- src/main/java/dk/easv/thorsmovieplayer/BLL/MovieManager.java
+++ src/main/java/dk/easv/thorsmovieplayer/BLL/MovieManager.java
@@ -1,4 +1,23 @@
 package dk.easv.thorsmovieplayer.BLL;
 
-public class MovieManager {
+import dk.easv.thorsmovieplayer.BE.Movie;
+import dk.easv.thorsmovieplayer.DAL.MovieDAO;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MovieManager
+{
+    private MovieDAO movieDAO;
+
+    public MovieManager() throws IOException
+    {
+        movieDAO = new MovieDAO();
+    }
+    public List<Movie> getAllMovies() throws SQLException
+    {
+        return movieDAO.getAllMovies();
+    }
+
 }
