872073b2564591bd5cad77e1f78734867d7d87cb
Frederik Lukas Godt Kjems <frel.g01@easv365.dk>
2026-01-09 12:33:33 +0100

    taget fra sqlite til movie dao

--- src/main/java/dk/easv/thorsmovieplayer/DAL/MovieDAO.java
+++ src/main/java/dk/easv/thorsmovieplayer/DAL/MovieDAO.java
@@ -1,14 +1,14 @@
 package dk.easv.thorsmovieplayer.DAL;
-//Project imports
+
+// Project imports
 import dk.easv.thorsmovieplayer.BE.Movie;
 
-//Java Imports
+// Java imports
+import java.io.IOException;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.IOException;
-import java.sql.SQLException;
 
 public class MovieDAO
 {
@@ -18,98 +18,105 @@ public class MovieDAO
     {
         this.dbConnector = DBConnector.getInstance();
     }
-    public Movie createMovie(Movie movie) throws SQLException{
-        String sql = "INSERT INTO Movie (title, filePath, lastView, personalRating, imdbRating) VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = dbConnector.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+    // CREATE
+    public Movie createMovie(Movie movie) throws SQLException
+    {
+        String sql = """
+            INSERT INTO Movie (title, filePath, lastView, personalRating, imdbRating)
+            VALUES (?, ?, ?, ?, ?)
+            """;
+
+        try (Connection conn = dbConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
         {
-        stmt.setString(1, movie.getTitle());
-        stmt.setString(2, movie.getFilePath());
-        stmt.setTimestamp(3, Timestamp.valueOf(movie.getLastView().atStartOfDay()));
-        stmt.setFloat(4, movie.getPersonalRating());
-        stmt.setFloat(5, movie.getImdbRating());
+            stmt.setString(1, movie.getTitle());
+            stmt.setString(2, movie.getFilePath());
 
-        stmt.executeUpdate();
+            if (movie.getLastView() != null)
+                stmt.setTimestamp(3, Timestamp.valueOf(movie.getLastView().atStartOfDay()));
+            else
+                stmt.setNull(3, Types.TIMESTAMP);
 
-        ResultSet rs = stmt.getGeneratedKeys();
-        if (rs.next()) {movie.setId(rs.getInt(1));}
+            stmt.setFloat(4, movie.getPersonalRating());
+            stmt.setFloat(5, movie.getImdbRating());
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next())
+                movie.setId(rs.getInt(1));
         }
         return movie;
     }
+
+    // READ ALL
     public List<Movie> getAllMovies() throws SQLException
     {
         List<Movie> movies = new ArrayList<>();
 
         String sql = """
-            SELECT id, title, filePath, lastView, personalRating, imdbRating FROM Movie
+            SELECT id, title, filePath, lastView, personalRating, imdbRating
+            FROM Movie
             """;
+
         try (Connection conn = dbConnector.getConnection();
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(sql))
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql))
         {
-            while (rs.next()){ movies.add(mapRowToMovie(rs));}
+            while (rs.next())
+                movies.add(mapRowToMovie(rs));
         }
         return movies;
     }
-    // Updates a movie in the SQLite database
-    public void updateMovie(Movie movie) throws Exception {
-        String sql = "UPDATE Movie SET title = ?, imdbRating = ?, personalRating = ?, filePath = ?, lastView = ? WHERE id = ?";
 
-        try (Connection conn = DriverManager.getConnection(DB_URL);
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+    // UPDATE
+    public void updateMovie(Movie movie) throws SQLException
+    {
+        String sql = """
+            UPDATE Movie
+            SET title = ?, imdbRating = ?, personalRating = ?, filePath = ?, lastView = ?
+            WHERE id = ?
+            """;
 
+        try (Connection conn = dbConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql))
+        {
             ps.setString(1, movie.getTitle());
             ps.setFloat(2, movie.getImdbRating());
             ps.setFloat(3, movie.getPersonalRating());
             ps.setString(4, movie.getFilePath());
 
-            if (movie.getLastView() != null) {
-                ps.setDate(5, Date.valueOf(movie.getLastView()));
-            } else {
-                ps.setNull(5, Types.DATE);
-            }
+            if (movie.getLastView() != null)
+                ps.setTimestamp(5, Timestamp.valueOf(movie.getLastView().atStartOfDay()));
+            else
+                ps.setNull(5, Types.TIMESTAMP);
 
             ps.setInt(6, movie.getId());
             ps.executeUpdate();
         }
     }
-    public void addMovie(Movie movie) throws Exception {
-        String sql = "INSERT INTO Movie(title, imdbRating, personalRating, filePath, lastView) VALUES(?,?,?,?,?)";
 
-        try (Connection conn = DriverManager.getConnection(DB_URL);
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-
-            ps.setString(1, movie.getTitle());
-            ps.setFloat(2, movie.getImdbRating());
-            ps.setFloat(3, movie.getPersonalRating());
-            ps.setString(4, movie.getFilePath());
-
-            if (movie.getLastView() != null) {
-                ps.setDate(5, Date.valueOf(movie.getLastView()));
-            } else {
-                ps.setNull(5, Types.DATE);
-            }
-
-            ps.executeUpdate();
-        }
-    }
-    // Deletes a movie from the SQLite database
-    public void deleteMovie(Movie movie) throws Exception {
+    // DELETE
+    public void deleteMovie(Movie movie) throws SQLException
+    {
         String sql = "DELETE FROM Movie WHERE id = ?";
 
-        try (Connection conn = DriverManager.getConnection(DB_URL);
-             PreparedStatement ps = conn.prepareStatement(sql)) {
-
+        try (Connection conn = DBConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql))
+        {
             ps.setInt(1, movie.getId());
             ps.executeUpdate();
         }
     }
-    private Movie mapRowToMovie(ResultSet rs) throws SQLException {
+
+    // MAPPER (MEGET VIGTIG)
+    private Movie mapRowToMovie(ResultSet rs) throws SQLException
+    {
         LocalDateTime lastView = null;
         Timestamp ts = rs.getTimestamp("lastView");
-        if (ts != null) {
+        if (ts != null)
             lastView = ts.toLocalDateTime();
-        }
 
         return new Movie(
                 rs.getInt("id"),
@@ -118,8 +125,6 @@ public class MovieDAO
                 rs.getFloat("personalRating"),
                 rs.getString("filePath"),
                 lastView
-
         );
     }
-
 }
