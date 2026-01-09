450d2fc7b332bcba19f6bcfb910c559277e73b3a
Frederik Lukas Godt Kjems <frel.g01@easv365.dk>
2026-01-09 12:23:00 +0100

    taget fra sqlite til movie dao

--- src/main/java/dk/easv/thorsmovieplayer/DAL/MovieDAO.java
+++ src/main/java/dk/easv/thorsmovieplayer/DAL/MovieDAO.java
@@ -73,7 +73,37 @@ public class MovieDAO
             ps.executeUpdate();
         }
     }
+    public void addMovie(Movie movie) throws Exception {
+        String sql = "INSERT INTO Movie(title, imdbRating, personalRating, filePath, lastView) VALUES(?,?,?,?,?)";
 
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, movie.getTitle());
+            ps.setFloat(2, movie.getImdbRating());
+            ps.setFloat(3, movie.getPersonalRating());
+            ps.setString(4, movie.getFilePath());
+
+            if (movie.getLastView() != null) {
+                ps.setDate(5, Date.valueOf(movie.getLastView()));
+            } else {
+                ps.setNull(5, Types.DATE);
+            }
+
+            ps.executeUpdate();
+        }
+    }
+    // Deletes a movie from the SQLite database
+    public void deleteMovie(Movie movie) throws Exception {
+        String sql = "DELETE FROM Movie WHERE id = ?";
+
+        try (Connection conn = DriverManager.getConnection(DB_URL);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, movie.getId());
+            ps.executeUpdate();
+        }
+    }
     private Movie mapRowToMovie(ResultSet rs) throws SQLException {
         LocalDateTime lastView = null;
         Timestamp ts = rs.getTimestamp("lastView");
